cf5eb7bc160b545fe6adf857e2be5787c3917d09
Roque <roquefsd@gmail.com>
2025-06-13 16:32:52 -0300

    v0.1.1 parse all text tags

--- index.js
+++ index.js
@@ -29,10 +29,29 @@ function html2uxml(name, h) {
 let tagMap = {
 	div: 'ui:VisualElement',
 	p: 'ui:Label',
+	span: 'ui:Label',
 	input: 'ui:TextField',
 	'input[type="text"]': 'ui:TextField',
 	'input[type="checkbox"]': 'ui:Toggle',
-	'text': 'ui:Label'
+	'text': 'ui:Label',
+	h1: 'ui:Label',
+	h2: 'ui:Label',
+	h3: 'ui:Label',
+	h4: 'ui:Label',
+	h5: 'ui:Label',
+	h6: 'ui:Label',
+	label: 'ui:Label',
+	strong: 'ui:Label',
+	b: 'ui:Label',
+	em: 'ui:Label',
+	i: 'ui:Label',
+	small: 'ui:Label',
+	mark: 'ui:Label',
+	abbr: 'ui:Label',
+	cite: 'ui:Label',
+	code: 'ui:Label',
+	q: 'ui:Label',
+	time: 'ui:Label'
 };
 
 function getElementTagName(element) {
